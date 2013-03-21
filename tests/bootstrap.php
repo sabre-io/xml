d4551ce86bd6574426fef49d7b9a2018c9f2126f
Evert Pot <evert@rooftopsolutions.nl>
2013-03-21 23:21:55 +0000

    Using the better composer autoloading mechanism.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,7 +1,16 @@
 <?php
 
-// Composer autoloader
-include __DIR__ . '/../vendor/autoload.php';
+$try = array(
+    __DIR__ . '/../vendor/autoload.php',
+    __DIR__ . '/../../../autoload.php',
+);
+
+foreach($try as $path) {
+    if (file_exists($path)) {
+        include $path;
+        break;
+    }
+}
 
 // Some extra classes
 include __DIR__ . '/Sabre/XML/Element/Mock.php';
