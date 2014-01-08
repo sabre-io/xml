f7fea22c5082fe68b2610e32dc933d0f2512e231
Evert Pot <evert@rooftopsolutions.nl>
2014-01-08 15:33:59 +0100

    Cleaned up CData element.

---
+++ tests/Sabre/XML/Element/CDataTest.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Sabre\XML\Element;
+
+use
+    Sabre\XML\Reader,
+    Sabre\XML\Writer;
+
+class CDataTest extends \PHPUnit_Framework_TestCase {
+
+    /**
+     * @expectedException \LogicException
+     */
+    function testDeserialize() {
+
+        $input = <<<BLA
+<?xml version="1.0"?>
+<root xmlns="http://sabredav.org/ns">
+ <blabla />
+</root>
+BLA;
+
+        $reader = new Reader();
+        $reader->elementMap = [
+            '{http://sabredav.org/ns}blabla' => 'Sabre\\XML\\Element\\CData',
+        ];
+        $reader->xml($input);
+
+        $output = $reader->parse();
+
+    }
+
+    function testSerialize() {
+
+        $writer = new Writer();
+        $writer->namespaceMap = [
+            'http://sabredav.org/ns' => null
+        ];
+        $writer->openMemory();
+        $writer->startDocument('1.0');
+        $writer->setIndent(true);
+        $writer->write([
+            '{http://sabredav.org/ns}root' => new Cdata('<foo&bar>'),
+        ]);
+
+        $output = $writer->outputMemory();
+
+        $expected = <<<XML
+<?xml version="1.0"?>
+<root xmlns="http://sabredav.org/ns"><![CDATA[<foo&bar>]]></root>
+
+XML;
+
+        $this->assertEquals($expected, $output);
+
+
+    }
+
+}
+
