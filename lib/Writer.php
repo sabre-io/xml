4e8b1b44a41585c2bdef3a10c68d434dc02166a5
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2014-11-24 12:54:17 +0100

    Update the exception message.

--- lib/Writer.php
+++ lib/Writer.php
@@ -112,7 +112,7 @@ class Writer extends XMLWriter {
                 if (is_int($name)) {
                     // This item has a numeric index. We expect to be an array with a name and a value.
                     if (!is_array($item) || !array_key_exists('name', $item) || !array_key_exists('value', $item)) {
-                        throw new InvalidArgumentException('When passing an array to ->write with numeric indices, every item must have a "name" and a "value"');
+                        throw new InvalidArgumentException('When passing an array to ->write with numeric indices, every item must be an array containing the "name" and "value" key');
                     }
 
                     $attributes = isset($item['attributes']) ? $item['attributes'] : [];
