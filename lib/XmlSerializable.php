e0a24932cbe448dde06217f9751acb74ce2b6116
Evert Pot <me@evertpot.com>
2015-02-27 22:16:09 -0500

    Corrected license link.

--- lib/XmlSerializable.php
+++ lib/XmlSerializable.php
@@ -10,7 +10,7 @@ use XMLReader;
  *
  * @copyright Copyright (C) 2013-2014 fruux GmbH (https://fruux.com/).
  * @author Evert Pot (http://evertpot.com/)
- * @license http://code.google.com/p/sabredav/wiki/License Modified BSD License
+ * @license http://sabre.io/license/ Modified BSD License
  */
 interface XmlSerializable {
 
