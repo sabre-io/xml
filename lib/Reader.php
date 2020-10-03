446ded78b07618ea2c437b891c4eb1b08e44ca05
Markus Staab <maggus.staab@googlemail.com>
2020-10-03 10:07:46 +0200

    Update lib/Reader.php

--- lib/Reader.php
+++ lib/Reader.php
@@ -55,6 +55,7 @@ class Reader extends XMLReader
      */
     public function parse(): array
     {
+        $previousEntityState = null;
         $shouldCallLibxmlDisableEntityLoader = (\PHP_VERSION_ID < 80000);
         if ($shouldCallLibxmlDisableEntityLoader) {
             $previousEntityState = libxml_disable_entity_loader(true);
