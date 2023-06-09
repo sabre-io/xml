7af6b61e868a79fa4892b46c91fbf505662044f9
Phil Davis <phil@jankaritech.com>
2023-06-09 14:49:59 +0545

    Apply cs-fixer fixes

--- tests/Sabre/Xml/ContextStack.php
+++ tests/Sabre/Xml/ContextStack.php
@@ -4,14 +4,12 @@ declare(strict_types=1);
 
 namespace Sabre\Xml;
 
-use XMLReader;
-
 /**
  * This is a wrapper around ContextStackTrait to use for unit tests.
  *
  * @license http://sabre.io/license/ Modified BSD License
  */
-class ContextStack extends XMLReader
+class ContextStack extends \XMLReader
 {
     use ContextStackTrait;
 }
